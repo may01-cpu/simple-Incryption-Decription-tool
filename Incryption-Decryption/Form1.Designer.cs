2642e263c5d2a7184bc755a21621d4100e6b559c
unknown <mayasbey16@gmail.com>
2025-06-30 23:13:02 +0100

    created the tool

---
+++ Incryption-Decryption/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace Incryption_Decryption
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TbEncrypt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TbDecrypt = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Calligraphy", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(246, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(450, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Encyption / Decryption";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Lucida Calligraphy", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(663, 533);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 56);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Decrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Lucida Calligraphy", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(152, 533);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 56);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Encrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(477, 194);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(3, 300);
+            this.panel1.TabIndex = 5;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.TbEncrypt);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Location = new System.Drawing.Point(12, 179);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(432, 315);
+            this.panel2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Lucida Calligraphy", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(20, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(224, 23);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "The encrypted text :";
+            // 
+            // TbEncrypt
+            // 
+            this.TbEncrypt.Font = new System.Drawing.Font("Lucida Calligraphy", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TbEncrypt.Location = new System.Drawing.Point(98, 228);
+            this.TbEncrypt.Name = "TbEncrypt";
+            this.TbEncrypt.ReadOnly = true;
+            this.TbEncrypt.Size = new System.Drawing.Size(226, 32);
+            this.TbEncrypt.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Calligraphy", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(20, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(409, 23);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Enter the text you wanna encrypt it :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Lucida Calligraphy", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(98, 88);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(226, 32);
+            this.textBox1.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Controls.Add(this.TbDecrypt);
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Controls.Add(this.textBox4);
+            this.panel3.Location = new System.Drawing.Point(509, 179);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(432, 315);
+            this.panel3.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Lucida Calligraphy", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(15, 181);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(223, 23);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "The decrypted text :";
+            // 
+            // TbDecrypt
+            // 
+            this.TbDecrypt.Font = new System.Drawing.Font("Lucida Calligraphy", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TbDecrypt.Location = new System.Drawing.Point(98, 228);
+            this.TbDecrypt.Name = "TbDecrypt";
+            this.TbDecrypt.ReadOnly = true;
+            this.TbDecrypt.Size = new System.Drawing.Size(226, 32);
+            this.TbDecrypt.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Lucida Calligraphy", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(15, 33);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(414, 23);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Enter the text you wanna Decrypt it :";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Lucida Calligraphy", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(98, 88);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(226, 32);
+            this.textBox4.TabIndex = 3;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(417, 125);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            999,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 26);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.Validating += new System.ComponentModel.CancelEventHandler(this.numericUpDown1_Validating);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Lucida Calligraphy", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(347, 125);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 23);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Key";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(966, 649);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TbEncrypt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TbDecrypt;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Incryption-Decryption/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Incryption_Decryption
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    
+
+        string EncryptText(string Text, short EncryptionKey)
+        {
+            char[] enc = Text.ToCharArray();
+            for (int i = 0; i < enc.Length; i++)
+            {
+                enc[i] = (char)(enc[i] + EncryptionKey);
+            }
+            return new string(enc);
+        }
+        string DecryptText(string Text, short EncryptionKey)
+        {
+            char[] enc = Text.ToCharArray();
+            for (int i = 0; i < enc.Length; i++)
+            {
+                enc[i] = (char)(enc[i] - EncryptionKey);
+            }
+            return new string(enc);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (!ValidateChildren()) return;
+            short key = (short)numericUpDown1.Value;
+            TbEncrypt.Text = EncryptText(textBox1.Text.Trim(), key);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) 
+        {
+            if(!ValidateChildren()) return;
+            short key = (short)numericUpDown1.Value;
+            TbDecrypt.Text = DecryptText(textBox4.Text.Trim(), key);
+        }
+
+        private void numericUpDown1_Validating(object sender, CancelEventArgs e)
+        {
+            if (numericUpDown1.Value <= 0)
+            {
+                e.Cancel = true;
+                numericUpDown1.Focus();
+                errorProvider1.SetError(numericUpDown1, "You must enter a positive key");
+            }
+            else
+            {
+                e.Cancel = false;
+                errorProvider1.SetError(numericUpDown1, "");
+             
+
+            }
+        }
+
+      
+    }
+}
